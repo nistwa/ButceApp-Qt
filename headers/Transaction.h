e9a347d28db2fd30aa729def881257b7a0d71cba
Nisanur Özpehlivan <126330681+nistwa@users.noreply.github.com>
2026-05-17 17:54:24 +0300

    Update Transaction.h

--- headers/Transaction.h
+++ headers/Transaction.h
@@ -1,1 +1,27 @@
+#ifndef TRANSACTION_H
+#define TRANSACTION_H
 
+#include <QString>
+#include <QDate>
+
+class Transaction {
+protected:
+    int id;
+    double amount;
+    QString category;
+    QDate date;
+
+public:
+    Transaction(double amt, QString cat, QDate dt) : amount(amt), category(cat), date(dt) {}
+    virtual ~Transaction() {}
+
+    // Saf sanal fonksiyon (Pure Virtual) - OOP kavramı!
+    virtual QString getType() const = 0; 
+    
+    // Getterlar
+    double getAmount() const { return amount; }
+    QString getCategory() const { return category; }
+    QDate getDate() const { return date; }
+};
+
+#endif
