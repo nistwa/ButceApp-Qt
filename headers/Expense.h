0223542e01a1acf9865fac05d10448e8085912a9
Nisanur Özpehlivan <126330681+nistwa@users.noreply.github.com>
2026-05-31 13:02:03 +0300

    Delete headers directory

--- headers/Expense.h
+++
@@ -1,7 +0,0 @@
-#include "Transaction.h"
-
-class Expense : public Transaction {
-public:
-    Expense(double amt, QString cat, QDate dt) : Transaction(amt, cat, dt) {}
-    QString getType() const override { return "Expense"; }
-};
